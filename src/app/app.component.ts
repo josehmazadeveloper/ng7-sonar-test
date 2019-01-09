5a44ea409f1af224ea7f0ee9e5389000783e7062
KRUGER\jmaza <jmaza@kruger.com.ec>
2019-01-09 11:42:20 -0500

    test sonar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MiClase } from '@services/otro/miclase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-sonar';
+  mensajeCustom: string;
+  ngOnInit() {
+    const jose = new MiClase();
+    this.mensajeCustom = jose.title;
+  }
+
 }
---
+++ src/app/services/otro/miclase.ts
@@ -0,0 +1,4 @@
+export class MiClase {
+  title = 'test-sonarxx';
+  constructor() {}
+}
